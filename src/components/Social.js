27243e06a16cc7258b194867a3b1178ac91ceb87
maneerat.m <maneeratmaneeon@gmail.com>
2021-06-07 12:38:16 +0700

    add link icon social

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
- class Social extends Component {
+class Social extends Component {
     render() {
         return (
             <div className="social">
-                <a href="#" target="_black" rel="noopender noreferrer">
-                    <i class="fab fa-github" href="https://github.com/maneeratmm"></i>
-                    <i class="fab fa-instagram" href="https://www.instagram.com/zionarty/"></i>
-                    <i class="fab fa-facebook" href="https://www.facebook.com/new.maneerat2534"></i>
+                <a href="https://github.com/maneeratmm" target="_black" rel="noopender noreferrer">
+                    <i class="fab fa-github" href="#"></i>
+                </a>
+                <a href="https://www.instagram.com/zionarty/" target="_black" rel="noopender noreferrer">
+                    <i class="fab fa-instagram" href="#"></i>
+                </a>
+                <a href="https://www.facebook.com/new.maneerat2534" target="_black" rel="noopender noreferrer">
+                    <i class="fab fa-facebook" href="#"></i>
                 </a>
             </div>
         )
